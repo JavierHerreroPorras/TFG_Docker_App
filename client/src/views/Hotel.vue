98e2d786a4333041b27e37cb23b7683560f75adc
Javier Herrero <javierherreroporras@gmail.com>
2021-01-31 19:04:20 +0100

    Added Axios and Docker to front-end

---
+++ client/src/views/Hotel.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Aquí se mostrarán los hoteles</h1>
+    <Hoteles/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Hoteles from '@/components/Hotel.vue'
+
+export default {
+  name: 'Hotel',
+  components: {
+    Hoteles
+  }
+}
+</script>
