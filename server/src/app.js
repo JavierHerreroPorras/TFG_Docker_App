98b3a84cd72b1f55042babbbcf5db4e0b5320109
Javier Herrero <javierherreroporras@gmail.com>
2021-02-21 19:53:23 +0100

    Finished docker app

--- server/src/app.js
+++ server/src/app.js
@@ -3,15 +3,25 @@
 const express = require('express');
 const bodyParser = require('body-parser'); 
 const cors = require('cors');
-
+const path = require('path');
 const app = express();
 
-
 const api = require('./routes');
+
+const CLIENT_BUILD_PATH = path.join(__dirname, "../../client/dist");
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 app.use('/api', api);
+
+// Static files
+app.use(express.static(CLIENT_BUILD_PATH));
+
+// Server React Client
+app.get("/", function(req, res) {
+    res.sendFile(path.join(CLIENT_BUILD_PATH , "index.html"));
+  });
+  
 
 module.exports = app;
